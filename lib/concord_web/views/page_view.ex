6f190114b97fa3ef6ae146c3f89dd92c45dc6663
Anders Pearson <anders@thraxil.org>
2023-02-09 21:51:33 +0000

    masonry

--- lib/concord_web/views/page_view.ex
+++ lib/concord_web/views/page_view.ex
@@ -1,3 +1,12 @@
 defmodule ConcordWeb.PageView do
   use ConcordWeb, :view
+  alias Concord.Photo
+
+  def thumb_url(%Photo{reticulum_key: reticulum_key, extension: extension, id: id}) do
+    "https://d2f33fmhbh7cs9.cloudfront.net/image/" <> reticulum_key <> "/960w/" <> Integer.to_string(id) <> extension
+  end
+
+  def large_url(%Photo{reticulum_key: reticulum_key, extension: extension, id: id}) do
+    "https://d2f33fmhbh7cs9.cloudfront.net/image/" <> reticulum_key <> "/960w/" <> Integer.to_string(id) <> extension
+  end
 end
