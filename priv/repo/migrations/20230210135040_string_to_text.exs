ce4f7c64f4d274d2be9eae52070c35037d65566d
Anders Pearson <anders@thraxil.org>
2023-02-10 13:58:24 +0000

    convert string to text for descriptions

---
+++ priv/repo/migrations/20230210135040_string_to_text.exs
@@ -0,0 +1,14 @@
+defmodule Concord.Repo.Migrations.StringToText do
+  use Ecto.Migration
+
+  def change do
+    alter table(:photos) do
+      modify :description, :text
+      modify :description_html, :text
+    end
+    alter table(:albums) do
+      modify :description, :text
+      modify :description_html, :text
+    end
+  end
+end
