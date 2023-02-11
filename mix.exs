c39611970973df8e5dc2ac085b0ed74558257e81
Anders Pearson <anders@thraxil.org>
2023-02-11 15:52:48 +0000

    fly deploy setup

--- mix.exs
+++ mix.exs
@@ -65,7 +65,10 @@ defmodule Concord.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": [
+        "tailwind default --minify",
+        "esbuild default --minify",
+        "phx.digest"]
     ]
   end
 end
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\concord" eval Concord.Release.migrate
